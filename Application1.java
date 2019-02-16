98ccf46560b6c3031db129cd14d5801227de9758
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-02-16 12:06:30 +0530

    one
    one

---
+++ Application1.java
@@ -0,0 +1,12 @@
+
+public class Application1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+    
+}
